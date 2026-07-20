f974c996826ecb824a3eff203fa8985e24b159b0
Samuel <samuelarul2001@gmail.com>
2026-07-20 09:11:49 +0530

    Add Microsoft Clarity tracking snippet

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -134,6 +134,13 @@ export default function RootLayout({ children }) {
             `,
           }}
         />
+        {/* Microsoft Clarity analytics */}
+        <script
+          id="clarity-init"
+          dangerouslySetInnerHTML={{
+            __html: `(function(c,l,a,r,i,t,y){c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);})(window, document, "clarity", "script", "xp7k5wqipw");`,
+          }}
+        />
         {/* Organization structured data */}
         <script
           type="application/ld+json"
